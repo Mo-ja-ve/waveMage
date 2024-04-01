796fc8ea47b9adebc0c7dc57b024db1f2afeb351
Mo-ja-ve <ak461717@ohio.edu>
2024-03-31 21:56:17 -0400

    first commit

---
+++ CMakeFiles/wavemage.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/backends/imgui_impl_opengl2.cpp.obj"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/backends/imgui_impl_opengl2.cpp.obj.d"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/backends/imgui_impl_sdl2.cpp.obj"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/backends/imgui_impl_sdl2.cpp.obj.d"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui.cpp.obj"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui.cpp.obj.d"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_demo.cpp.obj"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_demo.cpp.obj.d"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_draw.cpp.obj"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_draw.cpp.obj.d"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_tables.cpp.obj"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_tables.cpp.obj.d"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_widgets.cpp.obj"
+  "CMakeFiles/wavemage.dir/headers/include/imgui-master/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/wavemage.dir/main.cpp.obj"
+  "CMakeFiles/wavemage.dir/main.cpp.obj.d"
+  "libwavemage.dll.a"
+  "wavemage.exe"
+  "wavemage.exe.manifest"
+  "wavemage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wavemage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/wavemage.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wavemage.dir/main.cpp.obj"
+  "CMakeFiles/wavemage.dir/main.cpp.obj.d"
+  "libwavemage.dll.a"
+  "wavemage.exe"
+  "wavemage.exe.manifest"
+  "wavemage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wavemage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
